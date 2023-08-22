68ef758f8b66800397fcacaf1c280ba6e6b6e5d0
Thomas Ames <tham7107@colorado.edu>
2023-08-21 20:27:19 -0600

    Update AESD_ASSIGNMENTS_VERSION and AESD_CHAR_VERSION for Assignment 9

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_VERSION = 5807fb8c384bdd470ba5d6a252a9c6fab03256ed
+AESD_CHAR_VERSION = 7182d29ce430a65131904d5a89f6798dc2a9e22d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
